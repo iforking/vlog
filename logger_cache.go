e0c0db292f52c540d33a0ba5ad982cf083ede153
Liu Dong <liu.dong@qunar.com>
2017-06-15 14:42:37 +0800

    add global SetLevel/SetAppender/SetFormatter

--- logger_cache.go
+++ logger_cache.go
@@ -4,26 +4,81 @@ import (
 	"sync"
 )
 
-var logCache = &LoggerCache{logs: make(map[string]*Logger)}
+var logCache = &LoggerCache{loggerMap: make(map[string]*Logger)}
 
 type LoggerCache struct {
-	logs map[string]*Logger
-	lock sync.Mutex
+	loggerMap map[string]*Logger
+	lock      sync.Mutex
 }
 
 func (lc *LoggerCache) load(name string) *Logger {
 	lc.lock.Lock()
 	defer lc.lock.Unlock()
-	logger, ok := lc.logs[name]
+	logger, ok := lc.loggerMap[name]
 	if ok {
 		return logger
 	}
 
-	logger, ok = lc.logs[name]
+	logger, ok = lc.loggerMap[name]
 	if ok {
 		return logger
 	}
 	logger = createLogger(name)
-	lc.logs[name] = logger
+	lc.loggerMap[name] = logger
 	return logger
+}
+
+func (lc *LoggerCache) filter(prefix string) []*Logger {
+	loggers := []*Logger{}
+	lc.lock.Lock()
+	defer lc.lock.Unlock()
+	for _, logger := range lc.loggerMap {
+		if matchPrefix(logger.Name(), prefix) {
+			loggers = append(loggers, logger)
+		}
+	}
+	return loggers
+}
+
+func matchPrefix(name string, prefix string) bool {
+	if len(prefix) == 0 {
+		return true
+	}
+	if len(prefix) > len(name) {
+		return false
+	}
+	i := 0
+	for ; i < len(prefix); i++ {
+		if prefix[i] != name[i] {
+			return false
+		}
+	}
+	if prefix[i-1] == '/' || len(prefix) == len(name) || name[i] == '/' {
+		return true
+	}
+	return false
+}
+
+// set level of all loggers, which name has prefix segments, split by slash
+func SetLevel(prefix string, level Level) {
+	loggers := logCache.filter(prefix)
+	for _, logger := range loggers {
+		logger.SetLevel(level)
+	}
+}
+
+// set appender to all loggers, which name has prefix segments, split by slash
+func SetAppender(prefix string, appender Appender) {
+	loggers := logCache.filter(prefix)
+	for _, logger := range loggers {
+		logger.SetAppender(appender)
+	}
+}
+
+// set formatter, of all loggers, which name has prefix segments, split by slash
+func setFormatter(prefix string, formatter *Formatter) {
+	loggers := logCache.filter(prefix)
+	for _, logger := range loggers {
+		logger.SetFormatter(formatter)
+	}
 }
--- logger_cache_test.go
+++ logger_cache_test.go
@@ -6,10 +6,34 @@ import (
 )
 
 func TestLoad(t *testing.T) {
+	logCache := &LoggerCache{loggerMap: make(map[string]*Logger)}
 	logger1 := logCache.load("logger1")
 	logger2 := logCache.load("logger2")
 	logger3 := logCache.load("logger1")
 
 	assert.Equal(t, logger1, logger3)
 	assert.NotEqual(t, logger1, logger2)
+}
+
+func TestLoggerCache_Filter(t *testing.T) {
+	logCache := &LoggerCache{loggerMap: make(map[string]*Logger)}
+	_ = logCache.load("package0")
+	_ = logCache.load("package1")
+	_ = logCache.load("gopkg.in/package1")
+	_ = logCache.load("github.com/user1/package1")
+	_ = logCache.load("github.com/user2/package2")
+	_ = logCache.load("github.com/user3/package3")
+
+	loggers := logCache.filter("")
+	assert.Equal(t, 6, len(loggers))
+
+	loggers = logCache.filter("github.com")
+	assert.Equal(t, 3, len(loggers))
+
+	loggers = logCache.filter("github.com/user1/package1")
+	assert.Equal(t, 1, len(loggers))
+
+	loggers = logCache.filter("gopkg.in/")
+	assert.Equal(t, 1, len(loggers))
+	assert.Equal(t, "gopkg.in/package1", loggers[0].Name())
 }
